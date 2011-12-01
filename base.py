f2d31045e3f46dfa1dfb5f8e177061e1cc3ce6c6
Ben Lovell <ben.j.lovell@gmail.com>
2011-12-01 16:07:36 +0000

    Not real code, just an idea as to what to do

---
+++ base.py
@@ -0,0 +1,27 @@
+#Mostly pseudo code
+
+class Story():
+	uri = ""
+	comment_list = []
+	points = 0
+
+	def comment_on_story(comment_text):
+		#do comment if logged in
+
+class Comment():
+	username = ""
+	text = ""
+	subcomments = []
+	
+	def reply(comment_text):
+		#reply to this comment if logged in
+
+class Username():
+	username = ""
+	points = 0
+
+def login (username, password):
+	#do login
+
+def submit_story(story_url=None, story_title, story_text=None):
+	#blah
