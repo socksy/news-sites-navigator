0e4e2c81300d5c6dd315d72d146cab897d03c47b
Ben Lovell <ben.j.lovell@gmail.com>
2011-12-03 19:34:21 +0000

    Pseudopython -> real python

--- base.py
+++ base.py
@@ -1,27 +1,38 @@
 #Mostly pseudo code
 
 class Story():
-	uri = ""
-	comment_list = []
-	points = 0
 
-	def comment_on_story(comment_text):
-		#do comment if logged in
+    def __init__(self, uri, comment_list, points):
+        self.uri = uri
+        self.comment_list = comment_list
+        self.points = points
+
+    def comment_on_story(self, comment_text):
+        #do comment if logged in
+        pass
 
 class Comment():
-	username = ""
-	text = ""
-	subcomments = []
-	
-	def reply(comment_text):
-		#reply to this comment if logged in
+
+    def __init__(self, username, text, subcomments, points=None):
+        self.username = username
+        self.text = text
+        self.subcomments = subcomments
+        self.points = points
+
+    def reply(self, comment_text):
+        #reply to this comment if logged in
+        pass
 
 class Username():
-	username = ""
-	points = 0
+    def __init__(self, username, info=None, points=None):
+        self.username = username
+        self.info = info
+        self.points = points
 
 def login (username, password):
-	#do login
+    #do login
+    pass
 
-def submit_story(story_url=None, story_title, story_text=None):
-	#blah
+def submit_story(story_title, story_text=None, story_url=None):
+    #blah
+    pass
