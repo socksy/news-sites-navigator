a74920a0df76723940dc4fa6bd0a2152e6be27a8
Lin Chengyi <21982106+LinChengyi@users.noreply.github.com>
2011-12-11 04:55:44 +0000

    can now basicly display the comment, can't show replies, can't undisplay comment. But quite close

--- ui/storyview.py
+++ ui/storyview.py
@@ -52,24 +52,24 @@ class LinkText (urwid.Text):
         return True
 
     def keypress (self, size, key):
-        return key #TODO make this send a signal or something when clicked
+        return key #TODO make this send a signal or something when clicked 
 
 
 class StoryWidget (urwid.WidgetWrap):
     """Combines the LinkText widget and Votes widget in two columns."""
 
-    def __init__ (self, points, title):
-    	self.opened = False
-    	self.linktext = LinkText(title, wrap="space")
+    def __init__ (self, points, title, weight):
+        self.opened = False
+        self.title = title
+        self.linktext = LinkText(title, wrap="space")
         story_text = urwid.AttrWrap(self.linktext,
                                     'point_focus', 'focus')
-        story = urwid.Padding(story_text,
-                              'left', width=('relative',100), left=3)
+        story = urwid.Padding(story_text,'left', width=('relative',100), left=3)
         point_widget = VotesWidget(points)
         #internal variable so it can be kept track of for focus reasons
         self._both = urwid.AttrWrap(urwid.Columns(
                                         [point_widget, 
-                                        ('weight', 5, story)]),
+                                        ('weight', weight, story)]),
                                     'point body',
                                     'point focus')
 
@@ -97,16 +97,16 @@ class StoryView (object):
         self.storyList = self.r.load_stories("opensource")
 
         self.items = []
+        self.run_time_list = []
         for i in range(0,len(self.storyList.stories)):
-            self.items.append(StoryWidget(i+1, self.storyList.stories[i].text))
-    	
-    	urwid.connect_signal(up, 'click', self.on_click_up)
-    	urwid.connect_signal(down, 'click', self.on_click_down)
+            self.items.append(StoryWidget(i+1, self.storyList.stories[i].text, 10))
+            self.run_time_list.append((self.storyList.stories[i].storyID, None))
+        urwid.connect_signal(up, 'click', self.on_click_up)
+        urwid.connect_signal(down, 'click', self.on_click_down)
 
         self.listbox = urwid.ListBox(urwid.SimpleListWalker(self.items))
         self.view = urwid.Frame(urwid.AttrWrap(self.listbox, 'body'))
-        self.loop = urwid.MainLoop(self.view, self.palette, 
-                             unhandled_input=self.keystroke)
+        self.loop = urwid.MainLoop(self.view, self.palette, unhandled_input=self.keystroke)
         self.loop.run()
         
     def keystroke (self, input):
@@ -115,17 +115,14 @@ class StoryView (object):
         elif input in ['esc', ':']:
             self.set_command()
         elif input in ["enter"]:
-        	self.focus = self.listbox.get_focus()
-        	if isinstance(self.focus[0], StoryWidget):
-        		if self.focus[0].opened:
-        			story = self.storyList.stories[self.focus[1]]
-        			self.focus[0].linktext.set_text(story.text)
-        			self.focus[0].opened = False
-		       	else:
-		       		self.showComment(self.focus[1])
-		       		self.focus[0].opened = True
-		
-
+            self.focus = self.listbox.get_focus()
+            if isinstance(self.focus[0], StoryWidget):
+                if self.focus[0].opened:
+                    self.focus[0].opened = False
+                else:
+                    self.showComment(self.focus[1])
+                    self.focus[0].opened = True
+        			
     def set_command(self):
         self.footer = FooterEdit(':> ')
         self.view.set_footer(self.footer)
@@ -133,11 +130,11 @@ class StoryView (object):
         urwid.connect_signal(self.footer, 'entered', self.command)
         
     def on_click_up(self, button):
-		self.focus = self.listbox.get_focus()
-		story = self.storyList.stories[self.focus[1]]
-		storyID = story.storyID
-		result = self.r.vote(storyID, None, True)
-		self.view.set_footer(urwid.Text(result))	
+        self.focus = self.listbox.get_focus()
+        story = self.storyList.stories[self.focus[1]]
+        storyID = story.storyID
+        result = self.r.vote(storyID, None, True)
+        self.view.set_footer(urwid.Text(result))	
 
     def command(self, command):
         urwid.disconnect_signal(self, self.footer, 'entered', self.command)
@@ -149,11 +146,11 @@ class StoryView (object):
             self.view.set_focus('body')
             
     def on_click_down(self, button):
-		self.focus = self.listbox.get_focus()
-		story = self.storyList.stories[self.focus[1]]
-		storyID = story.storyID
-		result = self.r.vote(storyID, None, False)
-		self.view.set_footer(urwid.Text(result))	
+        self.focus = self.listbox.get_focus()
+        story = self.storyList.stories[self.focus[1]]
+        storyID = story.storyID
+        result = self.r.vote(storyID, None, False)
+        self.view.set_footer(urwid.Text(result))	
 
     def login(self):
         self.footer = FooterEdit(u'username: ')
@@ -176,29 +173,28 @@ class StoryView (object):
         	self.logged_in = "Login Successfull!"
         self.view.set_focus('body')
         self.view.set_footer(urwid.Text(self.logged_in))       
-    
-    def compose(self, comment_list, i):
-		result = ""
-		for a in range(i):
-			result = result + "    "
-		for com in comment_list:
-   			if len(com.subcomments) > 0:
-   				result += "--" + com.text + "\n" + self.compose(com.subcomments, i+1)
-   			else:
-   				for a in range(i):
-					result = result + "    "
-   				result += "--" + com.text + "\n"
-   		return result
-        
-    def showComment(self, i):
-    	story = self.storyList.stories[i]
-    	storyID = story.storyID
-    	comment_list = self.r.load_comments(storyID).comment_list
-    	content = story.text + "\nComment:\n"
-    	content += self.compose(comment_list, 0)
-    	self.focus[0].linktext.set_text(content)
-    
+
+    def get_text(self, comment_list):
+        result_list = []
+        for com in comment_list:
+            result_list.append(com.text)
+        return result_list
 	
+    def showComment(self, i):
+        storyID = self.run_time_list[i][0]
+        commentID = self.run_time_list[i][1]
+        comment_list = self.r.load_comments(storyID).comment_list
+        com_str_list = self.get_text(comment_list)
+        for com in com_str_list:
+            i = i + 1
+            self.items.insert(i, StoryWidget("Comment", com, 6))
+        self.listbox = urwid.ListBox(urwid.SimpleListWalker(self.items))
+        self.view = urwid.Frame(urwid.AttrWrap(self.listbox, 'body'))
+        self.loop.widget = self.view    	
+
+
+		
+
 
 if __name__ == '__main__':
     StoryView()
