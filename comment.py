065bd56d6c33f03bd0d68644cb291aece6d14232
Ben Lovell <ben.j.lovell@gmail.com>
2011-12-11 22:04:24 +0000

    Moved everything out of ui folder

---
+++ comment.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+
+import urwid
+
+class CommentTreeWidget(urwid.TreeWidget):
+    def display_text(self):
+        return self.get_node().get_value()['name']
+        
+class CommentNode(urwid.TreeNode):
+    def load_widget(self):
+        return CommentWidget(self)
+        
+class CommentParentNode(urwid.ParentNode):
+    def load_widget(self):
+        return CommentTreeWidget(self)
+    
+    def load_reply_keys(self):
+        value = self.get_value()
+        return range(len(data['reply']))
+        
+    def load_reply_node(self, key):
+        replydata = self.getvalue()['reply'][key]
+        replydepth = self.get_depth() + 1
+        if 'reply' in replydata:
+            replyclass = CommentParentNode
+        else:
+            replyclass = CommentNode
+        return replyclass(replydata, parent=self, key=key, depth=replydepth)
+        
+        
+class CommentBrowser:
+    palette = [
+        ('body', 'black', 'light gray'),
+        ('focus', 'light gray', 'dark blue', 'standout'),
+        ('head', 'yellow', 'black', 'standout'),
+        ('foot', 'light gray', 'black'),
+        ('key', 'light cyan', 'black','underline'),
+        ('title', 'white', 'black', 'bold'),
+        ('flag', 'dark gray', 'light gray'),
+        ('error', 'dark red', 'light gray'),
+        ]
+        
+    footer_text = [
+        ('title', "Comments"), "    ",
+        ('key', "UP"), ",", ('key', "DOWN"), ",",
+        ('key', "PAGE UP"), ",", ('key', "PAGE DOWN"),
+        "  ",
+        ('key', "+"), ",",
+        ('key', "-"), "  ",
+        ('key', "LEFT"), "  ",
+        ('key', "HOME"), "  ", 
+        ('key', "END"), "  ",
+        ('key', "Q"),
+        ]
+        
+    def __init__(self, data=None):
+        self.topnode = CommentParentNode
+        self.listbox = urwid.TreeListBox(urwid.TreeWalker(self.topnode))
+        self.listbox.offset_rows = 1
+        self.header = urwid.Text( "" )
+        self.footer = urwid.AttrWrap( urwid.Text( self.footer_text ),
+            'foot')
+        self.view = urwid.Frame(
+            urwid.AttrWrap( self.listbox, 'body' ),
+            header=urwid.AttrWrap(self.header, 'head' ),
+            footer=self.footer )
+            
+    def main(self):
+        self.loop = urwid.MainLoop(self.view, self.palette,
+            unhandled_input=self.quit_input)
+        self.loop.run()
+        
+    def quit_input(self, k):
+        if k in ('q','Q'):
+            raise urwid.ExitMainLoop()
+            
+def get_comment_tree():
+    retval = {"name":"comment1","reply":[]}
+    for i in range(10):
+        retval['reply'].append({"name":"troll"})
+        retval['reply'][i]['reply']=[]
+        retval['reply'][i]['reply'].append({"name":"troll2"})
+    return retval
+   
+def main():
+    tree = get_comment_tree()
+    CommentBrowser(tree).main()
+    
+    
+if __name__=="__main__": 
+    main()
+     
+     
---
+++ reddit_api.py
@@ -0,0 +1,100 @@
+import reddit
+import session
+import base
+
+class Reddit(session.Session):
+	
+	def __init__(self):
+		self.r = reddit.Reddit(user_agent = "null")
+		self.limit = 0
+		self.logged_in = False
+		self.info = "null"
+	
+	def login(self, ID, password):
+		self.r.login(user=ID,password=password)
+		if isinstance(self.r.user, reddit.redditor.LoggedInRedditor):
+			self.logged_in = True
+		return self.logged_in
+	
+	def load_stories(self, info):
+		self.info = info
+		self.limit += 5
+		submissions =  self.r.get_subreddit(info).get_hot(self.limit)
+		storyList = base.StoryList(info, [])
+		for sub in submissions:
+			story = base.Story(sub.id, sub.title, sub.permalink, [], sub.score)
+			storyList.stories.append(story)
+		return storyList
+	
+	def load_comments(self, storyID):
+		sub = self.r.get_submission(storyID)
+		story = base.Story(sub.id, sub.title, sub.permalink, [], sub.score)
+		for com in sub.comments:
+			reply_list = self.load_replies(com)
+			comment = base.Comment(com.id, com.author.name, com.body, reply_list)
+			story.comment_list.append(comment)
+		return story
+		
+	def load_replies(self, comment):
+		if len(comment.replies) > 0:
+			reply_list = []
+			for rep in comment.replies:
+				reply = base.Comment(rep.id, rep.author.name, rep.body, reply_list)
+				reply_list.append(reply)
+				reply.subcomments = self.load_replies(rep)
+			return reply_list
+		else:
+			return []
+		
+	def load_more(self):
+		return self.load_stories(self.info)
+	
+	def comment_on_story(self, storyID, comment_text):
+		if self.logged_in:
+			story = self.r.get_submission(storyID)
+			story.add_comment(comment_text)
+            		return True
+		else:
+			return "Error, please login first!"
+
+	def reply_to_comment(self, storyID, commentID, comment_text):
+		if self.logged_in:
+			story = self.r.get_submission(storyID)
+            		comment = None
+            		for com in story.comments:
+                		if comment == None:
+                    			comment = self.get_com(com, commentID)
+			comment.reply(comment_text)
+            		return True
+		else:
+			return "Error, please login first!"
+		
+	def vote(self, storyID, commentID, up):
+		if self.logged_in:
+            		story = self.r.get_submission(storyID)
+			if commentID == None:
+				if up:
+					story.upvote()
+				else:
+					story.downvote()
+			else:
+                		comment = None
+                		for com in story.comments:
+                    			if comment == None:
+                        			comment = self.get_com(com, commentID)
+				if up:
+					comment.upvote()
+				else:
+					comment.downvote()
+			return "OK"
+		else:
+			return "Error, please login first!"
+
+    	def get_com(self, comment, commentID):
+            result = None
+            for rep in comment.replies:
+                if rep.id == commentID:
+                    result = rep
+                else:
+                    result = self.get_com(rep, commentID)
+            return result
